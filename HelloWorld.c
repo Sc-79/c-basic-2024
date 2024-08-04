275c538c337ed4851a8b9fc6900c83e2de4da560
sourav <souravchongrey@gmail.com>
2024-08-04 22:52:28 +0530

    How to run c program and new line new tab , c program run terminal command

---
+++ HelloWorld.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    // How to run c programs
+    // 1. gcc HelloWorld.c
+    // 2. .\a.exe
+
+    // This is a comment
+   /*
+     This is multiple comment
+     on multiple lines
+     \n = new line
+     \t = tab
+   */
+
+    printf("Helloworld\n, world!\n");
+     printf("1\t2\t3\t4\n5\t6\t7\t8\n9\t10\t11\t12\n");
+    //  write double quotes
+    printf("\"Hello\" World");
+    return 0;
+}
