30bba08ab66c8bfc6a1d886da9f8c310adb87f36
sourav <souravchongrey@gmail.com>
2024-08-25 19:52:47 +0530

    add user input take from user and display

--- basic_c/AugmenterAssOp.c
+++ basic_c/AugmenterAssOp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// সি প্রোগ্রামিং ভাষায় অগমেন্টেড অপারেটরগুলি (অথবা সংক্ষেপে অরেথমেটিক অ্যাসাইনমেন্ট অপারেটর) এমন অপারেটর যা একটি ভেরিয়েবলের মানকে একটি নির্দিষ্ট মান দিয়ে পরিবর্তিত করে এবং সেই পরিবর্তিত মানটি আবার সেই ভেরিয়েবলে অ্যাসাইন করে। এগুলি মূলত কিছু অপারেশনকে সহজতর করতে ব্যবহার করা হয়।
 int main(){
     //  augumented assignment operator = used to replace a statement where an operator takes a variable as one of its arguments and then assigns the result back to the same variable 
         int x = 10;
--- basic_c/UserInput.c
+++ basic_c/UserInput.c
@@ -2,6 +2,10 @@
 
 int main(){
     char name[10];
+    int age;
+    printf("Enter your age:");
+    scanf("%d", &age);
+    printf("your age is %d", age);
     printf("Enter your name:");
     scanf("%s", &name);
     printf("your name is %s", name);
