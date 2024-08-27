cb7d86d68f29bd8d6e06fe61b14fde2d2839f774
sourav <souravchongrey@gmail.com>
2024-08-27 20:46:56 +0530

    basic program of pointer:

---
+++ basic_c/Pointer1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// basic painter program
+// pointer is a variable that stores the address of another variable
+
+ int main(){
+    printf("Hello World\n");
+    int x = 10;
+    int *a = &x;
+   printf("%d\n",*a);
+   int p = *a;
+   printf("%d\n",p);
+
+
+
+
+
+ }
--- basic_c/UserInput.c
+++ basic_c/UserInput.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    char name[10];
-    int age;
-    printf("Enter your age:");
-    scanf("%d", &age);
-    printf("your age is %d", age);
-    printf("Enter your name:");
-    scanf("%s", &name);
-    printf("your name is %s", name);
 
+  char name [30];
+fget(stdin, name, 30);
+
+
+   
 }
